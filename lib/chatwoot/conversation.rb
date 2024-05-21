6e854e6d294d479f10efaa10d14d95d658cb1c8e
AylineT <ayline.travers@gmail.com>
2024-05-21 16:04:38 +0200

    delete latest commit

--- lib/chatwoot/conversation.rb
+++ lib/chatwoot/conversation.rb
@@ -31,7 +31,7 @@ module Chatwoot
       # Get details of a specific conversation
       def details(conversation_id)
         raise "conversation_id is missing" unless conversation_id
-        response = Chatwoot.request(:get, "api/v1/accounts/#{Chatwoot.account_id}/conversations/#{conversation_id}/messages")
+        response = Chatwoot.request(:get, "api/v1/accounts/#{Chatwoot.account_id}/conversations/#{conversation_id}")
         response
       end
 
